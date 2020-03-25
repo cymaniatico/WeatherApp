b8eb0b929480c00a348d6a393436a721d0eb8c5d
cymaniatico <ingluisgarciac@gmail.com>
2020-03-25 16:36:27 -0500

    Array in Component ForecastExtended

--- src/components/ForecastExtended.js
+++ src/components/ForecastExtended.js
@@ -3,13 +3,28 @@ import PropTypes from 'prop-types';
 import './styles.css';
 import ForecastItem from './ForecastItem';
 
+const days = [
+    'Lunes',
+    'Martes',
+    'Miercoles',
+    'Jueves',
+    'Viernes',
+    'Sabado',
+    'Domingo'
+];
+
 class ForecastExtended extends Component{
+
+    renderForecastItemDays(){
+        return days.map(day=>(<ForecastItem weekDay={day}/>))//mapeo
+    }
+
     render(){
         const {city} = this.props;
         return(
         <div>
             <h2 className='forecast-title'>Pronóstico Extendido para {city}</h2>
-            <ForecastItem/>
+            {this.renderForecastItemDays()}
         </div>);
     }
 }
