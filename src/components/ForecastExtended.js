33c6d634ddf0173f4f5963fefcc3b3811270decd
cymaniatico <ingluisgarciac@gmail.com>
2020-03-25 21:41:45 -0500

    Fetch in ForecastExtended

--- src/components/ForecastExtended.js
+++ src/components/ForecastExtended.js
@@ -1,9 +1,9 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
-import './styles.css';
 import ForecastItem from './ForecastItem';
+import './styles.css';
 
-const days = [
+/*const days = [
     'Lunes',
     'Martes',
     'Miercoles',
@@ -13,18 +13,49 @@ const days = [
     'Domingo'
 ];
 
+const data = {
+    temperature: 10,
+    humidity: 10,
+    weatherState: 'normal',
+    wind: 'normal'
+}*/
+const api_key = "b6a1324d8cceb779bf86545ec7ec5bec";
+const url = "http://api.openweathermap.org/data/2.5/forecast";
+
 class ForecastExtended extends Component{
 
-    renderForecastItemDays(){
-        return days.map(day=>(<ForecastItem weekDay={day}/>))//mapeo
+    constructor() {
+        super();
+        this.state = {forecastData: null};
     }
 
+    componentDidMount() {
+        //fech or axios
+        const url_forecast =`${url}?q=${this.props.city}&appid=${api_key}`;
+        
+        fetch(url_forecast).then(
+            data => (data.json())
+        ).then(
+            weather_data => {
+                console.log(weather_data);
+            }
+        );
+    }
+    
+    renderForecastItemDays(){
+        return "Render Items";
+        //return days.map(day=>(<ForecastItem weekDay={day} hour={10} data = {data}/>))//mapeo
+    }
+    renderProgress = () =>{
+        return <h3>Cargando pronostico extendido...</h3>;
+    }
     render(){
         const {city} = this.props;
+        const {forecastData}= this.state;
         return(
         <div>
             <h2 className='forecast-title'>Pronóstico Extendido para {city}</h2>
-            {this.renderForecastItemDays()}
+            {forecastData ? this.renderForecastItemDays(): this.renderProgress()}
         </div>);
     }
 }
